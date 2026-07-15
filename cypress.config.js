323a1f0e380a7360a8a6316622fd9cba78ca91e4
Rudolf <ramazanov.rudolf@yandex.ru>
2026-07-15 10:39:53 +0500

    fv

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '8knsip',
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "http://qamid.tmweb.ru/client/index.php",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,47 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Cinema tests", () => {
+  const auth = require("../fixtures/auth.json");
+  const selectors = require("../fixtures/selectors.json");
+
+  it("Main page", () => {
+    cy.visit("/");
+    cy.get(selectors.header).should("be.visible");
+  });
+
+  it("Login successfull", () => {
+    cy.visit("http://qamid.tmweb.ru/admin");
+    cy.get(selectors.email).type(auth.login);
+    cy.get(selectors.password).type(auth.password);
+    cy.contains("Авторизоваться").click();
+    cy.get(selectors.admintext).should("have.text", "Администраторррская");
+  });
+
+  it("Login unsuccessfull", () => {
+    cy.visit("http://qamid.tmweb.ru/admin");
+    cy.get(selectors.email).type(auth.incorrectLogin);
+    cy.get(selectors.password).type(auth.password);
+    cy.contains("Авторизоваться").click();
+    cy.get(selectors.errortext).should("have.text", "Ошибка авторизации!");
+  });
+
+  it("Booking film", () => {
+    cy.visit("http://qamid.tmweb.ru/admin");
+    cy.get(selectors.email).type(auth.login);
+    cy.get(selectors.password).type(auth.password);
+    cy.contains("Авторизоваться").click();
+    cy.get(selectors.titlemovie)
+      .first()
+      .then(($el) => {
+        const movie1 = $el.text();
+        cy.log(movie1);
+        cy.visit("/");
+        cy.get(selectors.chooseday).click();
+        cy.get(selectors.titlemovie2)
+          .first()
+          .then(($el) => {
+            const movie2 = $el.text();
+            cy.log(movie2);
+            expect(movie2).to.contain(movie1);
+          });
+      });
+  });
+});
